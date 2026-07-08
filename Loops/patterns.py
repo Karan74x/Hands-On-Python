5bb2498073c1d4ac2a1391d72090d2855c76d6f9
Karan Rahani <karanrahani6@gmail.com>
2026-07-08 18:36:44 +0530

    Loops: added Pattern 2

--- Loops/patterns.py
+++ Loops/patterns.py
@@ -1,3 +1,9 @@
+#         *****
+#         *****
+#         *****
+#         *****
+#         *****
+
 def p1():
   for i in range(1,6):
     for j in range(1,6):
@@ -5,5 +11,19 @@ def p1():
 
     print()
 
-p1()
+# p1()
 
+
+#   *
+#   * *
+#   * * *
+#   * * * *
+#   * * * * *
+
+def p2():
+  for i in range(1,6):
+    for j in range(1,i+1):
+      print("*",end=" ")
+    print()
+
+p2()
