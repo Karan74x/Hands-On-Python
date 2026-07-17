eb2e3fa68c4a34c0a959c2b36df0238758d4e794
Karan Rahani <karanrahani6@gmail.com>
2026-07-17 19:49:23 +0530

    Data Science: DataFrame added 3 examples

--- PANDAS/DataScienceDay1.py
+++ PANDAS/DataScienceDay1.py
@@ -1,4 +1,10 @@
 import pandas as pd
+
+
+#                       Series
+#         A Series is a one-dimensional labeled array.
+
+
 # Example 1 :
 
 # marks = [85,78,39,87,13]
@@ -21,3 +27,33 @@ print(data["Anas"])
 
 # Accessing by position
 print(data[2])
+
+
+#                  DataFrame
+#       A DataFrame is a two-dimensional table.
+
+# Example 1:
+# students = {
+#   "names": ["Karan","Anas","Harsh","Saad","Aman","Arnold","Devda"],
+#   "marks": [74,66,49,35,25,30,8]
+# }
+
+# print(pd.DataFrame(students))
+
+
+# Example 2: DF using list of lists
+data = [
+  ["Karan",22],["Anas",66],["Harsh",49]
+]
+
+# print(pd.DataFrame(data, columns=["Name", "Age"]))
+
+
+# Example 3: DF using List of Dictionary
+sheet = [
+  {"Name": "Karan", "Age":22},
+  {"Name": "Anas", "Age":23},
+  {"Name": "Harsh", "Age":20}
+]
+
+print(pd.DataFrame(sheet))
