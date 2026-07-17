7847524f3a8ab93cf363b2e3e0de0a44aa7b3415
Karan Rahani <karanrahani6@gmail.com>
2026-07-17 19:32:54 +0530

    Data Science: Series example 3 custom index and accessing series

--- PANDAS/DataScienceDay1.py
+++ PANDAS/DataScienceDay1.py
@@ -5,6 +5,19 @@ import pandas as pd
 # data = pd.Series(marks)
 # print(data)
 
+
 # Example 2:
-names = ["Karan", "Anas", "Harsh"]
-print(pd.Series(names))
+# names = ["Karan", "Anas", "Harsh"]
+# print(pd.Series(names))
+
+
+# example 3: Custom Index and Accessing Series
+marks =[85,78,39]
+data = pd.Series(marks, index=["Karan", "Anas","Harsh"])
+print(data)
+
+# Accessing by index name
+print(data["Anas"])
+
+# Accessing by position
+print(data[2])
