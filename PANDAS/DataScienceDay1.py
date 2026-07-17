25c861ed81e11ba5022feb0ada93e4313fe34c48
Karan Rahani <karanrahani6@gmail.com>
2026-07-17 19:26:21 +0530

    Data Science: Series example 2

--- PANDAS/DataScienceDay1.py
+++ PANDAS/DataScienceDay1.py
@@ -1,5 +1,10 @@
 import pandas as pd
+# Example 1 :
 
-marks = [85,78,39,87,13]
-data = pd.Series(marks)
-print(data)
+# marks = [85,78,39,87,13]
+# data = pd.Series(marks)
+# print(data)
+
+# Example 2:
+names = ["Karan", "Anas", "Harsh"]
+print(pd.Series(names))
