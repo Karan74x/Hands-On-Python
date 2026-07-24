41ab7e2f53c986c98795be237549f35980dd1d21
Karan Rahani <karanrahani6@gmail.com>
2026-07-24 13:04:06 +0530

    refactor: simplify inner loop range in pattern program

--- Loops/patterns.py
+++ Loops/patterns.py
@@ -86,7 +86,7 @@ def p5():
 def p6():
   n= int(input("Enter the rows you want: "))
   for i in range(1, n+1):
-    for j in range(1, n-i+2):
+    for j in range(n-i+1):
       # 1st loop => 1 to 6(5-1+2)->  * * * * *
       # 2nd loop => 1 to 5(5-2+2)->  * * * *
       # 3rd loop => 1 to 4(5-3+2)->  * * *
