e44c505bf631857cd62e94623dde15cf42f41270
Karan Rahani <karanrahani6@gmail.com>
2026-07-24 11:21:34 +0530

    Implemented a proper triangle pattern

--- Loops/patterns.py
+++ Loops/patterns.py
@@ -33,6 +33,14 @@ def p2():
 #  **
 #  *
 
+# 1st way
+# for i in range(5,0,-1):
+#   print(i,end=" ")
+#   for j in range(1,i+1):
+#     print("*", end=" ")
+#   print()
+
+# 2nd way
 def p3():
   for i in range(1,6):
     for j in range(5-i+1):
@@ -154,3 +162,30 @@ def p9():
     print()
 
 # p9()
+
+
+
+  #         *
+  #       * * *
+  #     * * * * *
+  #   * * * * * * *
+  # * * * * * * * * *
+
+#  n = int(input("Enter the number of rows: "))
+def p10(n):
+
+    for i in range(1,n+1):
+  # printing spaces
+        for j in range(i,n+1):
+          print(" ",end=" ")
+
+        for k in range(1,i):
+          print("*", end=" ")
+
+        for l in range(1,i+1):
+          print("*", end=" ")
+
+        print()
+
+# p10(n)
+
