be8d7094652388b574845c4497dc9764000da839
Karan Rahani <karanrahani6@gmail.com>
2026-08-10 19:18:28 +0530

    Day 8: Practiced Pandas string functions

--- PANDAS/DataScienceDay8.py
+++ PANDAS/DataScienceDay8.py
@@ -10,8 +10,8 @@ fees = pd.read_csv(r"../CSV FILES/fees.csv")
 
 
 # StudentID → now becomes the index.
-students = students.set_index("StudentID")
-fees = fees.set_index("StudentID")
+# students = students.set_index("StudentID")
+# fees = fees.set_index("StudentID")
 
 
 # Keep all rows from students.
@@ -21,8 +21,44 @@ fees = fees.set_index("StudentID")
 # print(students.join(fees, how="right"))
 
 # how ="inner" ->Keep only IDs that exist in both tables.
-print(students.join(fees, how="inner"))
+# print(students.join(fees, how="inner"))
 
 
 # how="outer" -> Keep ALL rows from both DataFrames.
-print(students.join(fees, how="outer"))``
+# print(students.join(fees, how="outer"))
+
+
+
+#                String functions
+# Used to convert text to lowercase.
+print(students["Name"].str.lower())
+
+
+# Used to convert text to uppercase.
+print(students["Name"].str.upper())
+
+
+# Finds the length of each string.
+print(students["Name"].str.len())
+
+
+#emoves extra spaces from the beginning and end of a string.
+print(students["Name"].str.strip())
+
+
+# Used to check whether a string contains a particular word or character.
+print(students["Name"].str.contains("r", case=False)) #You can make it case-insensitive with case=False
+
+
+# Replace() -> Used to replace text inside strings.
+ print(students["City"].str.replace('a','7'))
+
+
+
+# startswith() -> Used to check whether a string starts with a particular character or text.
+print(students["Name"].str.startswith("R"))
+
+
+# str.endswith() checks whether a string ends with specific text.
+# print(students)
+print(students["City"].str.endswith('t'))
