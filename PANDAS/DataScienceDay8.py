81ffa766318d9729397d76a6347d45bf1e1b9750
Karan Rahani <karanrahani6@gmail.com>
2026-08-10 18:58:47 +0530

    Day 8: Practiced Pandas join() with left, right, and inner joins

---
+++ PANDAS/DataScienceDay8.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+students = pd.read_csv(r"../CSV FILES/students.csv")
+fees = pd.read_csv(r"../CSV FILES/fees.csv")
+
+
+#                  joins
+# merge() → usually joins using columns
+# join()  → joins using index
+
+
+# StudentID → now becomes the index.
+students = students.set_index("StudentID")
+fees = fees.set_index("StudentID")
+
+
+# Keep all rows from students.
+# print(students.join(fees, how="left"))
+
+#  Keep all rows from fees.
+# print(students.join(fees, how="right"))
+
+# inner()->Keep only IDs that exist in both tables.
+print(students.join(fees, how="inner"))
