cb1c89ac9c875cc7119337218f480d680d618d24
Karan Rahani <karanrahani6@gmail.com>
2026-08-14 18:25:45 +0530

    Day 1: Practice NumPy 3D arrays and element access

--- NumPy/NumpyDay1.py
+++ NumPy/NumpyDay1.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-# 1D array
+#                        1D array
+
 # arr = np.array([10, 20, 30, 40, 50])
 
 # # Accesing elements in 1D Array
@@ -9,19 +10,33 @@ import numpy as np
 
 
 
-# 2D array :
-arr = np.array([
-    [10, 20, 30],
-    [40, 50, 60],
-    [70, 80, 90]
-])
+#                         2D array :
+
+# arr = np.array([
+#     [10, 20, 30],
+#     [40, 50, 60],
+#     [70, 80, 90]
+# ])
 
 # Accessing elements
-print(arr[0]) #-> [10,20,30]
-print(arr[0][2]) #-> 30
-print(arr[1][1]) #-> 50
-print(arr[2][0]) #-> 70
+#print(arr[0]) #-> [10,20,30]
+# print(arr[0][2]) #-> 30
+# print(arr[1][1]) #-> 50
+# print(arr[2][0]) #-> 70
 
 # To check the dimension of the array
-print(arr.ndim)
+# print(arr.ndim)
 
+
+
+#                           3D array
+arr = np.array([
+              [['A','B','C'], ['D','E','F']],
+              [['G','H','i'], ['J','K','L']],
+              [['M','N','O'], ['P','Q','R']]
+              ]) # -> 3D array
+
+# Accessing elements in 3D array
+print(arr[2])
+print(arr[1][0][2])
+print(arr[0][1][1])
