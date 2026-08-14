5b7c4c432a610af223b724c32f7982f40de71a69
Karan Rahani <karanrahani6@gmail.com>
2026-08-14 18:20:35 +0530

    Day 1: Practice NumPy 2D arrays and element access

--- NumPy/NumpyDay1.py
+++ NumPy/NumpyDay1.py
@@ -1,8 +1,27 @@
 import numpy as np
 
 # 1D array
-arr = np.array([10, 20, 30, 40, 50])
+# arr = np.array([10, 20, 30, 40, 50])
 
-# Accesing elements in 1D Array
-print(arr[2])
-print(arr[-1])
+# # Accesing elements in 1D Array
+# print(arr[2])
+# print(arr[-1])
+
+
+
+# 2D array :
+arr = np.array([
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
+])
+
+# Accessing elements
+print(arr[0]) #-> [10,20,30]
+print(arr[0][2]) #-> 30
+print(arr[1][1]) #-> 50
+print(arr[2][0]) #-> 70
+
+# To check the dimension of the array
+print(arr.ndim)
+
