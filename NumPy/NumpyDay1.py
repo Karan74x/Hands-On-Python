490bbbc4b40e1af237b0becbda3bd1661a125a51
Karan Rahani <karanrahani6@gmail.com>
2026-08-22 22:21:21 +0530

    Day 1: Practice NumPy flatten and ravel

--- NumPy/NumpyDay1.py
+++ NumPy/NumpyDay1.py
@@ -46,14 +46,32 @@ arr = np.array([
               ])
 
 # Accessing elements in 3D array
-print(arr[2])
-print(arr[1][0][2])
-print(arr[0][1][1])
+# print(arr[2])
+# print(arr[1][0][2])
+# print(arr[0][1][1])
 
 #Another way to access elements
-print(arr[2,1,2])
+# print(arr[2,1,2])
 
 word = arr[2,0,0] + arr[0,0,0] + arr[2,0,1]
-print(word)
+# print(word)
 # Layer,rows,columns (3,2,3)
-print(arr.shape)
+# print(arr.shape)
+
+
+array = np.array([[10,20,30], [40,50,60]])
+
+# Converts a multidimensional array into 1D.
+# flatten() → always creates a copy
+flat = array.flatten()
+flat[0] = 100
+print(array)
+
+# ravel()   → returns a view when possible
+ravel_arr =array.ravel()
+ravel_arr[0] = 100
+print(array)
+
+
+
+
