4bb47c3550f8ca0379292cbad71c9fe4bc76dc96
Karan Rahani <karanrahani6@gmail.com>
2026-09-03 19:07:15 +0530

    practice pandas aggregation functions

--- PANDAS/SeparatePractices/revision5.py
+++ PANDAS/SeparatePractices/revision5.py
@@ -17,11 +17,77 @@ df = pd.DataFrame({
 
 # df.groupby("Department") to group by department
 
-for row in df.groupby("Department"):
-  print(row)
-
-# to group by Experience
-for row in df.groupby("Experience"):
-  print("\n",row)
+# for row in df.groupby("Department"):
+#   print(row)
 
 
+#           to group by Experience
+
+# for row in df.groupby("Experience"):
+#   print("\n",row)
+
+
+#                    1. sum()
+
+# is used to add up numeric values inside each group.
+# Because .sum() is applied to all columns:
+
+# Salary → numbers → added
+# Experience → numbers → added
+# Name → strings → concatenated
+
+print(df.groupby("Department").sum())
+
+
+
+
+#           only on a specific numeric column
+
+# groupby("Department") → make groups
+# ["Salary"]             → select Salary
+# .sum()                 → add Salary
+
+print(df.groupby("Department")
+    ["Salary"]
+    .sum())
+
+
+
+
+#                2.mean(numeric_only=True) = average.
+
+# Group employees by Department
+# Then calculate the average of the numeric columns in each department
+print(df.groupby("Department").mean(numeric_only=True))
+
+
+
+#                    median()
+#  means the middle value when the numbers are arranged in order.
+
+#          Group employees by Department
+# Then find the median (middle value) of the numeric columns
+
+print(df.groupby("Department").median(numeric_only=True))
+
+
+
+#         min() -> finds the smallest value in each group.
+
+# Group employees by Department
+# Then find the minimum value of each numeric column
+print(df.groupby("Department").min(numeric_only=True))
+
+
+
+#      max() -> finds the largest value in each group.
+
+# Group employees by Department
+# Then find the maximum value of each numeric column
+print(df.groupby("Department").max(numeric_only=True))
+
+
+#                   count()
+# tells us how many non-empty values exist in each group.
+
+print(df.groupby("Department").count())
